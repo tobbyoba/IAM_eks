b3620b7dcfc6779118e6d1395cc1837e477b6981
tobbyoba <tobbyoba86@gmail.com>
2023-10-11 19:40:47 -0500

    refactoring output

--- IAM_module/outputs.tf
+++ IAM_module/outputs.tf
@@ -1,8 +1,8 @@
 output "node_role" {
-  value = "aws_iam_role.demo.arn"
+  value = aws_iam_role.demo.arn
 }
 
 output "demo_role" {
-  value = "aws_iam_role.nodes.arn"
+  value = aws_iam_role.nodes.arn
 
 }
